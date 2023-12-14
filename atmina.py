365faa81d5644f64836951d674b3d698da35a917
Arminchiks <firedemons51@gmail.com>
2023-12-14 14:54:46 +0200

    3.stunda
    3.stunda

--- atmina.py
+++ atmina.py
@@ -10,18 +10,18 @@ bgImg=ImageTk.PhotoImage(Image.open("7.png"))
 #.resize((200.200),Image.ANTIALIAS)
 
 #Izveido pogas - jābūt pāra skaitlim
-btn0=Button(width=200,height=300,image=bgImg) #sāk no 0, jo sasaistās ar saraksta elementu indeksāciju
-btn1=Button(width=200,height=300,image=bgImg)
-btn2=Button(width=200,height=300,image=bgImg)
-btn3=Button(width=200,height=300,image=bgImg)
-btn4=Button(width=200,height=300,image=bgImg)
-btn5=Button(width=200,height=300,image=bgImg)
-btn6=Button(width=200,height=300,image=bgImg)
-btn7=Button(width=200,height=300,image=bgImg)
-btn8=Button(width=200,height=300,image=bgImg)
-btn9=Button(width=200,height=300,image=bgImg)
-btn10=Button(width=200,height=300,image=bgImg)
-btn11=Button(width=200,height=300,image=bgImg)
+btn0=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn0,0)) #sāk no 0, jo sasaistās ar saraksta elementu indeksāciju
+btn1=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn1,1))
+btn2=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn2,2))
+btn3=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn3,3))
+btn4=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn4,4))
+btn5=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn5,5))
+btn6=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn6,6))
+btn7=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn7,7))
+btn8=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn8,8))
+btn9=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn9,9))
+btn10=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn10,10))
+btn11=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn11,11))
 
 #kopē pogas, maina skaitli
 
@@ -49,6 +49,40 @@ myImg5=ImageTk.PhotoImage(Image.open("5.png"))
 myImg5=ImageTk.PhotoImage(Image.open("6.png"))
 
 
+ImageList=[myImg1,myImg1,myImg2,myImg2,myImg3,myImg3,myImg4,myImg4,myImg5,myImg5]#attelus ievieto masiivaa
+
+random.shuffle(ImageList)#sajauc attelus
+
+
+count=0
+correctAnswers=0
+answers=[]
+answer_dict={}#ar iekavam nezinu
+
+def btnClick(btn,number):
+    global count, correctAnswers,answers,answer_dict
+    if btn["image"]=="pyimage1" and count<2:#peec sisteemas nnosauc saadi
+        btn["image"]==ImageList[number]
+        count+=1
+        answers.append(number)#bez s iespejams
+        answer_dict[btn]=ImageList[number]#iespejams kantainaas
+    if len(answers)==2:#ja atveras divas kartites
+        if ImageList[answers[0]]==ImageList[answers[1]]:#salidzina attelus
+            for key in answer_dict:
+                key["state"]=DISABLED
+            correctAnswers+=2
+            if correctAnswers==2:
+                messagebox.showinfo("Vienadi attēli")
+
+            else:
+                messagebox.showinfo("Nav vienadi atteli")
+                for key in answer_dict:
+                    key["image"]="pyimage1"
+                count = 0
+                answers=[]
+                answer_dict={}
+
+    return 0
 
 
 gameWindow.mainloop()

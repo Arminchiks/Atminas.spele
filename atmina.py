6833eb2446316b253e240f1fa5b8278b0fca2cea
Arminchiks <firedemons51@gmail.com>
2024-01-11 15:14:25 +0200

    beigas
    beigas

--- atmina.py
+++ atmina.py
@@ -9,6 +9,68 @@ gameWindow=Tk()
 gameWindow.title("Vienādie attēli")
 bgImg=ImageTk.PhotoImage(Image.open("7.png"))
 #.resize((200.200),Image.ANTIALIAS)
+galvenaIzvelne=Menu(gameWindow)
+gameWindow.config(menu=galvenaIzvelne)
+
+def infoLogs():
+    gameWindow=Toplevel()
+    gameWindow.title("Par programmu")
+    gameWindow.geometry('1055x200')
+    desc=Label(gameWindow,text="Spēles mērķis ir iegūt vienādas bildes zem apslēptiem lodziņiem.",font='Helvita 26 bold')
+    desc.grid(row=0,column=0)
+    desc=Label(gameWindow,text="Uzspiežot uz jebkuru tu izvēlies bildi un tad tev ir jāuzspiež uz citu lodziņu,",font='Helvita 16')
+    desc.grid(row=1,column=0)
+    desc=Label(gameWindow,text="ja bildes nesakrīt, tad aizvērsies izvēlētās bildītes un būs viss jāizvēlas vēlreiz,",font='Helvita 16')
+    desc.grid(row=2,column=0)
+    desc=Label(gameWindow,text="ja bildes sakritīs, tad bildes paliks atvērtas un būs jāizvēlas jaunas, līdz attaisīsi visas bildītes",font='Helvita 16')
+    desc.grid(row=3,column=0)
+def reset():
+    global count,correctAnswers,answers,answer_dict
+    btn0.config(state=NORMAL)
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn10.config(state=NORMAL)
+    btn11.config(state=NORMAL)
+
+    btn0['image']='pyimage1'
+    btn1['image']='pyimage1'
+    btn2['image']='pyimage1'
+    btn3['image']='pyimage1'
+    btn4['image']='pyimage1'
+    btn5['image']='pyimage1'
+    btn6['image']='pyimage1'
+    btn7['image']='pyimage1'
+    btn8['image']='pyimage1'
+    btn9['image']='pyimage1'
+    btn10['image']='pyimage1'
+    btn11['image']='pyimage1'
+    random.shuffle(ImageList)
+    answers=[]
+    answer_dict={}
+    count=0
+    correctAnswers=0
+    return 0
+
+opcijas=Menu(galvenaIzvelne,tearoff=False)
+galvenaIzvelne.add_cascade(label='Opcijas',menu=opcijas)
+
+opcijas.add_command(label='Jauna spēle',command=reset)
+opcijas.add_command(label='Iziet',command=gameWindow.quit)
+
+
+galvenaIzvelne.add_command(label='Par programmu',command=infoLogs)
+
+
+#def
+
+
 
 #Izveido pogas - jābūt pāra skaitlim
 btn0=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn0,0)) #sāk no 0, jo sasaistās ar saraksta elementu indeksāciju
@@ -74,18 +136,18 @@ def btnClick(btn,number):
             correctAnswers+=2
             if correctAnswers==2:
 
-                messagebox.showinfo("Vienadi attēli")
+                messagebox.showinfo("Vienādi attēli",'Tu uzminēji')
                 correctAnswers=0
 
 
         else:
-            messagebox.showinfo("Nav vienadi atteli")
+            messagebox.showinfo("Nav vienādi atteli",'Tu neuzminēji')
             for key in answer_dict:
                 key["image"]="pyimage1"
         count = 0
         answers=[]
         answer_dict={}
-                
+            
 
     return 0
 
